b888d867ac57176b0c355df87a839c38d7f7be7b
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-27 20:05:06 +0800

    Add files via upload

---
+++ lft/EvaristeGalois/matrixM.ts
@@ -0,0 +1,14 @@
+import './x123456.js'
+import './y123456.js'
+import './z123456.js'
+import './matrixX.js'
+import './matrixY.js'
+import './matrixZ.js'
+void function matrixM(){
+  void  'a + b + c + d + e + f'
+  void  'a - b - c - d - e - f'
+  void  'a * b * c * d * e * f'
+  void  'a % b % c % d % e % f'
+  void  'a / b / c / d / e / f'
+  void  'a ^ b ^ c ^ d ^ e ^ f'
+}
