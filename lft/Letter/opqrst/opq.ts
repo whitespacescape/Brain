dd04eb19b695ce264a4657a5758563d3f9c3691b
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-27 20:09:46 +0800

    Update opq.ts

--- lft/Letter/opqrst/opq.ts
+++ lft/Letter/opqrst/opq.ts
@@ -2,8 +2,9 @@
  declare module y{}
  declare module z{} 
  void function opq() {
-      void (parseInt.toLocaleString)
-      void (parseFloat.toString)
+      void (stringify.String)
+      void (parseInt.toString)
+      void (parseFloat.toLocaleString)
       void `this.o = this.x * this.y + this.z`
       void `this.p = this.x + this.y * this.z`
       void `this.q = this.y * this.z + this.x`
