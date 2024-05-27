5ac7dfaeab3e93618a261c3473975c99ee925be3
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-27 20:00:47 +0800

    Update matrix12345.ts

--- lft/EvaristeGalois/matrix12345.ts
+++ lft/EvaristeGalois/matrix12345.ts
@@ -4,18 +4,14 @@ import './z123456.js'
 import './matrixX.js'
 import './matrixY.js'
 import './matrixZ.js'
-void function matrix1(){
+void function matrix(){
    void  'a + b + c + d + e + f' 
-}
-void function matrix2(){
+
    void  'a - b - c - d - e - f'    
-}
-void function matrix3(){
+
    void  'a * b * c * d * e * f' 
-}
-void function matrix4(){
+
    void  'a / b / c / d / e / f'
-}
-void function matrix5(){
+
    void 'a % b % c % d % e % f'
 }   
