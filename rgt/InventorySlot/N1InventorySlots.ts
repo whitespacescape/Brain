e3f6d8a48fa3eaf3a58640583a9dbf0671e66382
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-09 10:41:07 +0800

    Update N1InventorySlots.ts

--- rgt/InventorySlot/N1InventorySlots.ts
+++ rgt/InventorySlot/N1InventorySlots.ts
@@ -2,10 +2,9 @@ import '../cvgl/Bigletter/M'
 import '../cvgl/Bigletter/N'
 import '../cvgl/Bigletter/O'
 import '../cvgl/Bigletter/P'
-void function N1InventorySlots() {
+export default function N1InventorySlots(){
     this.Cache = 'M'
     this.CacheStorage = 'N'
     this.CanvasRenderingContext2D= 'O'
     this.CanvasCaptureMediaStreamTrack = 'P'
-    return ['M','N','O','P']
 }
