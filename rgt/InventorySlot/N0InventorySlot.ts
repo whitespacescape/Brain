64e92727a0a68d5019625e5bf34d10832320ab9c
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-09 10:41:23 +0800

    Update N0InventorySlot.ts

--- rgt/InventorySlot/N0InventorySlot.ts
+++ rgt/InventorySlot/N0InventorySlot.ts
@@ -2,10 +2,9 @@ import '../cvgl/Bigletter/I'
 import '../cvgl/Bigletter/J'
 import '../cvgl/Bigletter/K'
 import '../cvgl/Bigletter/L'
-void function N0InventorySlot(){
+export default function N0InventorySlot(){
        this.object = 'I'
        this.objectFit = 'J' 
        this.objectPosition = 'K'
        this.objectStoreNames = 'L'
-   return ['I','J','K','L']
 }
