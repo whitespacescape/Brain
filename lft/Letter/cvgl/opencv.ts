12114b6f5ff2ade2b019e11cf4c5a2789fa270f0
ace <15368104568@189.cn>
2024-06-09 10:52:14 +0800

    Delete lft/Letter/cvgl/opencv.ts

--- lft/Letter/cvgl/opencv.ts
+++
@@ -1,8 +0,0 @@
- //visual lib
-import '../cvgl/BigLetter'
-void function opencv(i:string,j:StringConstructor,k:FunctionStringCallback){
-void "i%j/k"
-void "j%k/l"
-void "l%j/i"
-return [i,j,k] 
- }
