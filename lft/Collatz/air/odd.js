2c0e4620dd4832681859d8857e41852ccde0008d
ace <15368104568@189.cn>
2024-06-17 15:38:37 +0800

    Update odd.js

--- lft/Collatz/air/odd.js
+++ lft/Collatz/air/odd.js
@@ -3,7 +3,7 @@ import '../tree/tree_i'
 import '../tree/tree_ii'
 export function odd(){
     void function OddNumber(oddNumber){
-        if (oddNumber % 2 === 0) {
+        if (oddNumber % 2 == 0) {
             oddNumber = oddNumber + 1;
         } else {
             oddNumber = oddNumber - 1;
